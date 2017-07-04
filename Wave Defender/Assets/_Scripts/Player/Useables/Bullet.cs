bf71f8c41c2f30f4296a687a218ac076024a157f
DeesV <dees.veen@ehldeventer.nl>
2017-07-04 10:40:00 +0200

    Last Changes

--- Wave Defender/Assets/_Scripts/Player/Useables/Bullet.cs
+++ Wave Defender/Assets/_Scripts/Player/Useables/Bullet.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 // Deze class staat op elke bullet die geinstantiate wordt in de Guns Class. Dit regelt dat de kogel vooruit vliegt en kapot gaat zodra het moet.
 public class Bullet : MonoBehaviour {
 
+    BulletKind myTestBullet = new BulletKind(30, 35);
+
     // De update roept de juiste move functie aan.
     void Update() {
         moveBullet();
@@ -13,7 +15,7 @@ public class Bullet : MonoBehaviour {
 
     // De move functie zorgt alleen maar voor movement in de kogel.
     void moveBullet() {
-        transform.GetComponent<Rigidbody>().velocity = transform.forward * 30;
+        transform.GetComponent<Rigidbody>().velocity = transform.forward * myTestBullet.speed;
     }
 
     //Deze functie zorgt ervoor dat de kogel kapot gaat zodra hij een ander object aanraakt.
@@ -21,5 +23,15 @@ public class Bullet : MonoBehaviour {
         Destroy(gameObject);
     }
 
+}
+
+public class BulletKind {
+    public float speed;
+    public float damage;
+
+    public BulletKind(float _speed,float _damage) {
+        speed = _speed;
+        damage = _damage;
+    }
 }
 
